76ffcf3cc5bd95d66adf6d44fdb519ee5dd23e5b
Doug Begui <Lorynbegui@gmail.com>
2026-09-12 20:21:17 -0300

    fix: serverExternalPackages e config de deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   agentRules: false,
+  serverExternalPackages: ["bullmq", "@valkey/valkey-glide"],
 };
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
